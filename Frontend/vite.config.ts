afbf661252996ede5d1be69919ff38dd66b875d3
Sambhav-3010 <sambhavmani.tripathi@gmail.com>
2026-01-30 03:28:56 +0530

    Frontend > Fixing cross-origin policy for web containers

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -16,4 +16,10 @@ export default defineConfig({
             'Cross-Origin-Opener-Policy': 'same-origin',
         },
     },
+    preview: {
+        headers: {
+            'Cross-Origin-Embedder-Policy': 'require-corp',
+            'Cross-Origin-Opener-Policy': 'same-origin',
+        },
+    },
 })
